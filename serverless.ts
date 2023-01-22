c34ad1f5fa50c269063596b9f5055c68b19d5ff0
Cleginaldo <cleginaldo3000@gmail.com>
2023-01-22 20:52:05 -0300

    feat: add puppeteer for generate pdf

--- serverless.ts
+++ serverless.ts
@@ -11,6 +11,7 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'us-east-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
@@ -18,6 +19,22 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: ['dynamodb:*'],
+            Resource: ['*'],
+          },
+          {
+            Effect: 'Allow',
+            Action: ['s3:*'],
+            Resource: ['*'],
+          },
+        ],
+      },
     },
   },
   // import the function via paths
@@ -47,6 +64,7 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ['chrome-aws-lambda'],
     },
     dynamodb: {
       stages: ['dev', 'local'],
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,4 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+import chromium from 'chrome-aws-lambda';
+import dayjs from 'dayjs';
+import { readFileSync } from 'fs';
+import { compile } from 'handlebars';
+import { join } from 'path';
 
 import { document } from '../utils/dynamodbClient';
 
@@ -8,20 +14,24 @@ interface ICreateCertificate {
   grade: string;
 }
 
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+
+  const html = readFileSync(filePath, 'utf-8');
+
+  return compile(html)(data);
+};
+
 export const handler: APIGatewayProxyHandler = async event => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
-
-  await document
-    .put({
-      TableName: 'users_certificate',
-      Item: {
-        id,
-        name,
-        grade,
-        created_at: new Date().getTime(),
-      },
-    })
-    .promise();
 
   const response = await document
     .query({
@@ -33,8 +43,72 @@ export const handler: APIGatewayProxyHandler = async event => {
     })
     .promise();
 
+  const userAlreadyExits = response.Items[0];
+
+  if (!userAlreadyExits) {
+    await document
+      .put({
+        TableName: 'users_certificate',
+        Item: {
+          id,
+          name,
+          grade,
+          created_at: new Date().getTime(),
+        },
+      })
+      .promise();
+  }
+
+  const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png');
+  const medal = readFileSync(medalPath, 'base64');
+
+  const data: ITemplate = {
+    name,
+    id,
+    date: dayjs().format('DD/MM/YYYY'),
+    grade,
+    medal,
+  };
+
+  const content = await compileTemplate(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? './certificate.pdf' : null,
+  });
+
+  await browser.close();
+
+  const s3 = new S3();
+
+  await s3
+    .putObject({
+      Bucket: 'certificatereactjsqueroserdev',
+      Key: `${id}.pdf`,
+      ACL: 'public-read',
+      Body: pdf,
+      ContentType: 'application/pdf',
+    })
+    .promise();
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0]),
+    body: JSON.stringify({
+      message: 'Certificado criado com sucesso!',
+      url: `https://certificatereactjsqueroserdev.s3.amazonaws.com/${id}.pdf`,
+    }),
   };
 };
